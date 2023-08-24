d34a69a6c10f7596e54e78756d28cb67014f3608
DavidConselvan <dmconselvan@gmail.com>
2023-08-24 09:07:56 -0300

    handout faltando apenas ex 7

--- exemplo_de_uso.py
+++ exemplo_de_uso.py
@@ -1,6 +1,15 @@
 from database import Database
-from database import Note
+from database import Note   
 db = Database('banco')
 
 db.add(Note(title='Pão doce', content='Abra o pão e coloque o seu suco em pó favorito.'))
 db.add(Note(title=None, content='Lembrar de tomar água'))
+
+notes = db.get_all()
+for note in notes:
+    print(f'Anotação {note.id}:\n  Título: {note.title}\n  Conteúdo: {note.content}\n')
+
+new_note = Note(2, 'ola', 'oi tudo bem')
+db.update(new_note)
+
+db.delete('3')
